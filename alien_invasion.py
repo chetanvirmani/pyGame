2b962f72a4a2e5aae9e186c1fff3436d166afe0a
Chetan Virmani <chetan.virmani16@gmail.com>
2022-02-24 20:10:11 -0600

    Full screen and Q to exit

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,7 +13,9 @@ class alienInvasion:
         self.settings = settings()
 
         # pygame.display.set_mode() is a surface - a surface is a part of screen where the game elements will be displayed
-        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         
         pygame.display.set_caption("Alien Invasion")
 
@@ -27,36 +29,43 @@ class alienInvasion:
 
         while True:
 
-            #self.checkEvents()
+            self.checkEvents()
             self.ship.update()
-            #def checkEvents(self): #Respond to keypresses and mouse events
-            for event in pygame.event.get(): #event is a press of a key by the user
-                if event.type == pygame.QUIT: #We'll write different IF statements to figure out the type of event the user requested
-                    sys.exit() #When player clicks the close window button, this will kick in and the game will be exited
-                
-                elif event.type == pygame.KEYDOWN: #If the user has pressed a key
-                    if event.key == pygame.K_RIGHT: #Checking for the key type
-                        self.ship.movingRight = True #Move the ship along the x axis
-                    elif event.key == pygame.K_LEFT:
-                        self.ship.movingLeft = True
-
-                elif event.type  == pygame.KEYUP:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.movingRight = False
-                    elif event.key == pygame.K_LEFT:
-                        self.ship.movingLeft = False
-
-
-
-
-            #self.updateScreen()
-
-            #def updateScreen(self):
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme() #Ship appears on top of the background
-            pygame.display.flip() #makes the most recently drawn screen visible, so in the while loop it continously displays the results of events
+            self.updateScreen()
 
             
-            
 
+    def checkEvents(self): #Respond to keypresses and mouse events
+
+        for event in pygame.event.get(): #event is a press of a key by the user
+            if event.type == pygame.QUIT: #We'll write different IF statements to figure out the type of event the user requested
+                sys.exit() #When player clicks the close window button, this will kick in and the game will be exited
+            
+            elif event.type == pygame.KEYDOWN:
+                self.checkKeydownEvents(event)
+            
+            elif event.type == pygame.KEYUP:
+                self.checkKeyupEvents(event)
+        
+    
+    def updateScreen(self):
+    
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme() #Ship appears on top of the background
+        pygame.display.flip() #makes the most recently drawn screen visible, so in the while loop it continously displays the results of events
+
+    def checkKeydownEvents (self, event):
+        if event.key == pygame.K_RIGHT: #Checking for the key type
+            self.ship.movingRight = True #Move the ship along the x axis
+        elif event.key == pygame.K_LEFT:
+            self.ship.movingLeft = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    
+    def checkKeyupEvents (self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.movingRight = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.movingLeft = False
 
